a7084555adcc1c88e0fcb917541df9d52dec5160
vardjaklap <alexperesta@gmail.com>
2019-02-27 19:21:38 -0500

    Updates

---
+++ precache-manifest.2613e0e2d352c2deed7f3330e2c226f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deadb9e176394db301e1",
+    "url": "/Fitonia2/static/css/main.6191b70e.chunk.css"
+  },
+  {
+    "revision": "deadb9e176394db301e1",
+    "url": "/Fitonia2/static/js/main.deadb9e1.chunk.js"
+  },
+  {
+    "revision": "bb10fcfcf55db025b65a",
+    "url": "/Fitonia2/static/js/1.bb10fcfc.chunk.js"
+  },
+  {
+    "revision": "4e8355ac6a50667053ff",
+    "url": "/Fitonia2/static/js/runtime~main.4e8355ac.js"
+  },
+  {
+    "revision": "3792346d2a273d0c14007847a21650b0",
+    "url": "/Fitonia2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Fitonia2/precache-manifest.a7227f90b7d61d34a216f8c31ad09fac.js"
+  "/Fitonia2/precache-manifest.2613e0e2d352c2deed7f3330e2c226f7.js"
 );
 
 workbox.clientsClaim();
